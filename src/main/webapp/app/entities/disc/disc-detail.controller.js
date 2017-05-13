b6639d3265c6de5f840bd23679dafbb044668768
mhbratek <mhbratek@gmail.com>
2017-05-13 19:13:17 +0200

    1

--- src/main/webapp/app/entities/disc/disc-detail.controller.js
+++ src/main/webapp/app/entities/disc/disc-detail.controller.js
@@ -25,5 +25,38 @@
             vm.disc = result;
         });
         $scope.$on('$destroy', unsubscribe);
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+        $scope.datasetOverride = [{
+            yAxisID: 'y-axis-1'
+        }, {
+            yAxisID: 'y-axis-2'
+        }];
+        $scope.options = {
+            scales: {
+                yAxes: [{
+                    id: 'y-axis-1',
+                    type: 'linear',
+                    display: true,
+                    position: 'left'
+                }, {
+                    id: 'y-axis-2',
+                    type: 'linear',
+                    display: true,
+                    position: 'right'
+                }]
+            }
+        };
+        $scope.labels1 = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+        $scope.series1 = ['Series A', 'Series B'];
+
+        $scope.data1 = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
     }
 })();
